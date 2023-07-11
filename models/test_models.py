e1dc6af002361fda3b5460cf343f8e40fd830c81
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-07-11 19:23:28 -0300

    [DOCUMENTATION] Grid results

--- models/test_models.py
+++ models/test_models.py
@@ -11,10 +11,10 @@ models = [
         model_name='Logistic Regression',
         model=LogisticRegression(),
         model_parameter_grid={
-            'C': [0.1, 0.5, 1.0, 2.0, 5.0],
-            'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
-            'max_iter': [200, 500, 1000],
-            'n_jobs': [-1],
+            'C': [0.1, 0.5, 1.0, 2.0],
+            'solver': ['newton-cg', 'lbfgs'],
+            'max_iter': [50, 100, 200, 500],
+            #'n_jobs': [-1],
             'random_state': [42],
         },
     ),
@@ -22,43 +22,34 @@ models = [
         model_name='Random Forest',
         model=RandomForestClassifier(),
         model_parameter_grid={
-            'n_estimators': [100, 200, 500, 1000],
-            'criterion': ['gini', 'entropy', 'log_loss'],
-            'max_depth': [None, 10, 20, 50, 100],
+            'n_estimators': [100, 200, 500],
+            'criterion': ['gini', 'entropy'],
+            'max_depth': [None, 10, 20, 50],
+            'random_state': [42],
         },
     ),
     Model(
         model_name='KNN',
         model=KNeighborsClassifier(),
         model_parameter_grid={
-            'n_neighbors': [3, 5, 7, 9, 11, 13, 15],
+            'n_neighbors': [3, 5, 9, 15],
             'weights': ['distance'],
             'p': [1, 2],
-            'n_jobs': [-1],
+            #'n_jobs': [-1],
         },
     ),
     Model(
         model_name='SVM',
         model=SVC(),
         model_parameter_grid={
-            'C': [0.1, 0.5, 1.0, 2.0, 5.0],
+            'C': [0.1, 0.5, 1.0, 2.0],
             'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
-            'degree': [1, 2, 3, 4, 5],
+            'degree': [1, 3, 5],
             'gamma': ['scale', 'auto'],
             'max_iter': [200, 500, 1000],
             'random_state': [42],
         },
     ),
-    Model(
-        model_name='Decision Tree',
-        model=DecisionTreeClassifier(),
-        model_parameter_grid={
-            'criterion': ['gini', 'entropy'],
-            'splitter': ['best'],
-            'max_depth': [None, 5, 10, 50, 100],
-            'random_state': [42],
-        },
-    )
 ]
 
 models_small = [
@@ -66,9 +57,9 @@ models_small = [
         model_name='Decision Tree',
         model=DecisionTreeClassifier(),
         model_parameter_grid={
-            'criterion': ['gini'],
+            'criterion': ['gini', 'entropy'],
             'splitter': ['best'],
-            'max_depth': [None],
+            'max_depth': [None, 5, 10, 50, 100],
             'random_state': [42],
         },
     )
